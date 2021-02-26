b9c8ec1a9c4d97061f8e066098b11061d51ef5f8
brongh <brongh90@gmail.com>
2021-02-26 22:16:33 +0800

    need to do up the jwt for login

---
+++ client/src/context/reducer.js
@@ -0,0 +1,42 @@
+
+
+export const initialState = {
+    userDetails: "" || userDetails,
+    token: "" || token,
+    loading: false,
+    errorMessage: null,
+  };
+  
+  export const AuthReducer = (initialState, action) => {
+    switch (action.type) {
+      case "REQUEST_LOGIN":
+        return {
+          ...initialState,
+          loading: true,
+        };
+      case "LOGIN_SUCCESS":
+        return {
+          ...initialState,
+          token: action.payload.access,
+          userDetails: jwt(action.payload.access),
+          
+          loading: false,
+        };
+      case "LOGOUT":
+        return {
+          ...initialState,
+          userDetails: "",
+          token: "",
+        };
+  
+      case "LOGIN_ERROR":
+        return {
+          ...initialState,
+          loading: false,
+          errorMessage: action.error,
+        };
+  
+      default:
+        throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  };
